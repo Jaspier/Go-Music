6e40ecb0986ca4ce056c2f632437efb90d3b3b33
Jaspier <dungoallen349@gmail.com>
2021-12-03 19:15:15 +0000

    Routing to components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Music from './components/Music';
+import Home from './components/Home';
+import Admin from './components/Admin';
 
 export default function App() {
   return (
@@ -20,7 +23,7 @@ export default function App() {
                   <Link to='/music'>Music</Link>
                 </li>
                 <li className='list-group-item'>
-                  <Link to='/admin'>Manage Songs</Link>
+                  <Link to='/admin'>Manage Collection</Link>
                 </li>
               </ul>
             </nav>
@@ -42,16 +45,4 @@ export default function App() {
       </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function Music() {
-  return <h2>Music</h2>;
-}
-
-function Admin() {
-  return <h2>Manage Songs</h2>;
 }
---
+++ src/components/Music.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Music extends Component {
+  render() {
+    return <h2>Choose Song</h2>;
+  }
+}
