536e89555b754efda850c753d432fd184d81b9c2
Jaspier <dungoallen349@gmail.com>
2021-12-05 16:27:48 +0000

    Getting list of movies from backend and implemented basic error checking

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -2,31 +2,58 @@ import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Music extends Component {
-  state = { movies: [] };
+  state = {
+    songs: [],
+    isLoaded: false,
+    error: null,
+  };
 
   componentDidMount() {
-    this.setState({
-      movies: [
-        { id: 1, title: 'Fair Trade', duration: 193 },
-        { id: 2, title: 'Love Nwantiti', duration: 203 },
-        { id: 3, title: 'Dancing in the Kitchen', duration: 630 },
-      ],
-    });
+    fetch('http://localhost:4000/v1/songs')
+      .then(response => {
+        if (response.status !== '200') {
+          let err = Error;
+          err.message = 'Invalid response code: ' + response.status;
+          this.setState({ error: err });
+        }
+        return response.json();
+      })
+      .then(json => {
+        this.setState(
+          {
+            songs: json.songs,
+            isLoaded: true,
+          },
+          error => {
+            this.setState({
+              isLoaded: true,
+              error,
+            });
+          }
+        );
+      });
   }
 
   render() {
-    return (
-      <Fragment>
-        <h2>Choose Song</h2>
+    const { songs, isLoaded, error } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <p>Loading...</p>;
+    } else {
+      return (
+        <Fragment>
+          <h2>Choose Song</h2>
 
-        <ul>
-          {this.state.movies.map(m => (
-            <li key={m.id}>
-              <Link to={`/songs/${m.id}`}>{m.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </Fragment>
-    );
+          <ul>
+            {songs.map(m => (
+              <li key={m.id}>
+                <Link to={`/songs/${m.id}`}>{m.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </Fragment>
+      );
+    }
   }
 }
